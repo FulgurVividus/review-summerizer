d6cb37ea2822257e94f2cf34281151ced7125e95
murodjon <murodjon.muzaffarov1@gmail.com>
2025-10-16 23:25:07 +0500

    feat: handled edge cases

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,5 +1,7 @@
+import { reviewRepository } from "./../repositories/review.repository";
 import type { Request, Response } from "express";
 import { reviewService } from "../services/review.service";
+import { productRepository } from "../repositories/product.repository";
 
 export const reviewController = {
   async getReviews(req: Request, res: Response) {
@@ -13,12 +15,24 @@ export const reviewController = {
     const reviews = await reviewService.getReviews(productId);
     res.json(reviews);
   },
-
+  //
   async summarizeReviews(req: Request, res: Response) {
     const productId: number = Number(req.params.id);
 
     if (isNaN(productId)) {
       res.status(400).json({ error: "Invalid product ID" });
+      return;
+    }
+
+    const product = await productRepository.getProduct(productId);
+    if (!product) {
+      res.status(400).json({ error: "Invalid product" });
+      return;
+    }
+
+    const reviews = await reviewRepository.getReviews(productId, 1);
+    if (!reviews.length) {
+      res.status(400).json({ error: "There are no reviews to summarize" });
       return;
     }
 
