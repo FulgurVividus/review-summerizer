e2e845ab571f616e785d931b2ccff8211f1431d5
murodjon <murodjon.muzaffarov1@gmail.com>
2025-10-20 15:47:59 +0500

    feat: displaying the summary

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import StarRating from "./StarRating";
+import { Button } from "../ui/button";
+import { HiSparkles } from "react-icons/hi2";
 
 type Props = {
   productId: number;
@@ -58,17 +59,33 @@ const ReviewList = ({ productId }: Props) => {
     );
   }
 
+  if (!reviewData?.reviews.length) {
+    return null;
+  }
+
   return (
-    <div className="flex flex-col gap-5">
-      {reviewData?.reviews.map((review) => (
-        <div key={review.id}>
-          <div className="font-semibold">{review.author}</div>
-          <div>
-            <StarRating value={review.rating} />
+    <div>
+      <div className="mb-5">
+        {reviewData?.summary ? (
+          <p>{reviewData.summary}</p>
+        ) : (
+          <Button type="button">
+            <HiSparkles /> Summarize
+          </Button>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-5">
+        {reviewData?.reviews.map((review) => (
+          <div key={review.id}>
+            <div className="font-semibold">{review.author}</div>
+            <div>
+              <StarRating value={review.rating} />
+            </div>
+            <p className="py-2">{review.content}</p>
           </div>
-          <p className="py-2">{review.content}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
